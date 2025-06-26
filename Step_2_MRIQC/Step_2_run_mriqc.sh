56efc09cc9233a63db77c01c0a09be3cfaec35b9
shanshan717 <zhushanshan0717@gmail.com>
2025-06-26 21:20:46 +0800

    update mriqc tutorial --ss

--- Step_2_MRIQC/Step_2_run_mriqc.sh
+++ Step_2_MRIQC/Step_2_run_mriqc.sh
@@ -1,69 +1,33 @@
 #!/bin/bash
-bids_root_dir=/mnt/sdb1/Judge-fMRI-Data/bids
-output_dir=/mnt/sdb1/Judge-fMRI-Data/MRIQC_SS
+
+# User inputs:
+bids_root_dir=/Volumes/ss/fMRI_Pipeline/BIDS
+subj=$1
 nthreads=6
-mem=24 # memory in GB
+mem=24  # memory in GB
 
-# List of participant IDs
-participants=("036" "037" "038" "039" "040" "043" "044" "045" 
-"046" "047" "049" "050" "051" "052" "053" "054" "055")
+# Define output path
+output_dir=$bids_root_dir/derivatives/mriqc
 
-total=${#participants[@]}  
-count=0  
+# Run MRIQC
+echo ""
+echo "Running MRIQC on participant: sub-$subj"
+echo ""
 
-# Progress bar function
-progress_bar() {
-  local progress=$1
-  local total=$2
-  local width=50  
-  local percent=$(( progress * 100 / total ))
-  local completed=$(( progress * width / total ))
-  local remaining=$(( width - completed ))
+# Make output directories if they don't exist
+mkdir -p $output_dir/sub-${subj}
 
-  printf "["
-  for ((i = 0; i < completed; i++)); do
-    printf "="
-  done
-  for ((i = 0; i < remaining; i++)); do
-    printf " "
-  done
-  printf "] %d%% (%d/%d)\r" $percent $progress $total
-}
+# Run MRIQC with the provided Docker image and settings
+docker run -it --rm \
+  -v $bids_root_dir:/data:ro \
+  -v $output_dir/sub-${subj}:/out \
+  poldracklab/mriqc:latest /data /out participant \
+  --participant_label $subj \
+  --n_proc $nthreads \
+  --mem_gb $mem \
+  --float32 \
+  --ants-nthreads $nthreads \
+  -w /out \
+  --verbose-reports \
+  --no-sub
 
-# Loop through each participant
-for subj in "${participants[@]}"
-do
-  count=$((count + 1))  
-
-  echo ""
-  echo "Running MRIQC on participant: sub-$subj"
-  echo ""
-
-  # Create the MRIQC derivatives folder and participant-specific folder if they don't exist
-  if [ ! -d $output_dir ]; then
-    mkdir -p $output_dir
-  fi
-
-  if [ ! -d $output_dir/sub-${subj} ]; then
-    mkdir -p $output_dir/sub-${subj}
-  fi
-
-  # Run MRIQC with the provided Docker image and settings
-  docker run -it --rm \
-    -v $bids_root_dir:/data:ro \
-    -v $output_dir/sub-${subj}:/out \
-  ss/mriqc /data /out participant \
-    --participant_label $subj \
-    --n_proc $nthreads \
-    --mem_gb $mem \
-    --float32 \
-    --ants-nthreads $nthreads \
-    -w /out \
-    --verbose-reports \
-    --no-sub
-
-  progress_bar $count $total
-  echo ""  
-done
-
-echo "All participants processed."
